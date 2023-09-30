ce34101329503b171363303c6c47e64a1f2c0e6b
Ralla <andi_ralla@hotmail.com>
2023-09-30 20:22:37 -0300

    LoopWhile

---
+++ LoopWhile.sql
@@ -0,0 +1,48 @@
+
+-- Anderson R Oliveira / 24.09.2023
+
+-- loop While para realizar o tuning de queries para extracoes massivas de grande periodo de datas. 
+
+DECLARE @DataInicio DATE = '2023-01-01';
+DECLARE @DataFinal DATE = CONVERT(DATE, GETDATE() - 1);
+DECLARE @DataAtual DATE;
+
+SET @DataAtual = @DataInicio
+
+CREATE TABLE #Datas (Data DATE);
+
+WHILE @DataInicio <= @DataFinal
+
+BEGIN
+    
+	INSERT INTO #Datas (Data) VALUES (@DataInicio);
+    SET @DataInicio = DATEADD(DAY, 1, @DataInicio);
+
+PRINT('Data Atual ' + CONVERT(NVARCHAR(10), @DataAtual, 120))
+
+
+IF OBJECT_ID('Tempdb..#Temp1') IS NOT NULL DROP TABLE #Temp1;
+
+SELECT
+    a.[Data],
+    CONVERT(VARCHAR(2), a.[Hora], 108) AS 'Hora',
+    a.[Coluna1],
+    SUM(a.[Coluna2]) AS 'SomaColuna2',
+    SUM(a.[Coluna3]) AS 'SomaColuna3',
+    SUM(a.[Coluna4]) AS 'SomaColuna4',
+    SUM(a.[Coluna5]) AS 'SomaColuna5'
+INTO #Temp1
+FROM [BANCO_DADOS].[dbo].[TABELA] a WITH (NOLOCK)
+INNER JOIN #Datas AS b
+ON a.[Data] = b.Data
+GROUP BY
+    a.[Data], CONVERT(VARCHAR(2), a.[Hora], 108), a.[Coluna1];
+
+	SET @DataAtual = DATEADD(DAY, 1, @DataAtual); 
+
+END;
+
+SELECT * FROM #Temp1 a
+INNER JOIN #Datas b
+ON a.[Data] = b.Data
+ORDER BY a.[Data] ASC;
